56408a8d363455429b48a7ff34ee58307ae8ace1
boghdady <boghdady107@gmail.com>
2020-09-16 13:51:35 +0200

    create User,Company and Map Classes

---
+++ src/User.ts
@@ -0,0 +1,18 @@
+import faker from 'faker';
+
+export class User {
+	name: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+
+	constructor() {
+		// Initialization
+		this.name = faker.name.findName();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude())
+		};
+	}
+}
